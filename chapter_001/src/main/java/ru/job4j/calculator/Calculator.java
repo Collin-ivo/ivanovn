b72750a0349288102821fed0299fc5fc28609591
Collin-ivo <n_13@bk.ru>
2018-03-06 19:52:27 +0300

    3.1. Элементарный калькулятор. [#185]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,10 +1,29 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculator
+ *
+ * @author Nikolay Ivanov (n_13@bk.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculator {
     private double result;
 
     public void add(double first, double second) {
         this.result = first + second;
+    }
+
+    public void subtract(double first, double second) {
+        this.result = first - second;
+    }
+
+    public void div(double first, double second) {
+        this.result = first / second;
+    }
+
+    public void multiple(double first, double second) {
+        this.result = first * second;
     }
 
     public double getResult() {
--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -14,6 +14,33 @@ public class CalculatorTest {
         double result = calc.getResult();
         double expected = 2D;
         assertThat(result, is(expected));
-        }
+    }
+
+    @Test
+    public void whenAddTwoPlusOneThenOne() {
+        Calculator calc = new Calculator();
+        calc.subtract(2D, 1D);
+        double result = calc.getResult();
+        double expected = 1D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenAddOneDivOneThenOne() {
+        Calculator calc = new Calculator();
+        calc.div(2D, 2D);
+        double result = calc.getResult();
+        double expected = 1D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenAddOneMultipleOneThenFour() {
+        Calculator calc = new Calculator();
+        calc.multiple(2D, 2D);
+        double result = calc.getResult();
+        double expected = 4D;
+        assertThat(result, is(expected));
+    }
 
 }
